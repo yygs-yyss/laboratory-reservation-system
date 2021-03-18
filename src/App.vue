a6724c0678415efd7a626688b6c74010064d30f2
ygs <420546720@qq.com>
2021-03-18 20:48:14 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <sidebar id="sidebar" />
     <router-view id="router1" :key="$router.path" />
   </div>
   <div>
     <login />
+  </div>
+  <br />
+  <br /> -->
+  <div>
+    <setup />
   </div>
 </template>
 
@@ -12,8 +17,9 @@
 import { defineComponent } from "vue";
 import sidebar from "@/views/Sidebar.vue";
 import login from "@/views/example03/Login.vue";
+import setup from "@/views/example02/Example02.vue";
 export default defineComponent({
-  components: { sidebar, login }
+  components: { setup }
 });
 </script>
 
